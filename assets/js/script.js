00eb666a435b5eebf0234aaee91a327a4cd9b2f5
Anisha McDowell <anishamcdowell@gmail.com>
2021-03-06 17:21:39 -0500

    Local storage saving correctly. Search hx displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,20 @@ var myKey = "a590260e0a66edf311e2ebae9ded9db5";
 //HTML ELEMENTS
 var searchBoxEl = $("#search-box");
 var searchButtonEl = $("#search-btn");
-var searchedCity = searchBoxEl.val();
+var searchedCity;
+var searchHxEl = $("#search-hx");
 
-//Current storage
-localStorage.getItem("city");
+
+//STORAGE DATA
+localStorage.getItem(searchedCity);
 
 //When the user types in a city and hits the search button
 searchButtonEl.click(function(e) {
     e.preventDefault();
+    searchedCity = searchBoxEl.val();
     console.log(searchedCity);
-    localStorage.setItem("city", searchedCity);
+    localStorage.setItem(searchedCity, searchedCity);
+    searchHxEl.append(`<p>${searchedCity}`);
 
     // //Name of the city they are searching is appended to API url parameters
     // var cityUrl = ""
@@ -30,8 +34,6 @@ searchButtonEl.click(function(e) {
     //     .then (function(data){
     //         console.log(data);
     //     })
-
-    // //Name of searched city is stored in local storage
 
     // //Main city data appears in HTML document
 
